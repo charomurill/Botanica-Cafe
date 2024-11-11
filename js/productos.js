cce9545ca489f7c164a6d545bb4a9bc05cde3af2
charomurill <charomurill@gmail.com>
2024-11-11 15:31:34 -0300

    Primer commit

---
+++ js/productos.js
@@ -0,0 +1,20 @@
+const productos = [
+    {
+        nombre: 'Café Argentino',
+        precio: 3000,
+        img: 'img/paqueteCafeArgentino.jpg',
+        descripcion: 'Nacional y popular'
+    },
+    {
+        nombre: 'Café Brasilero',
+        precio: 3800,
+        img: 'img/paqueteCafe2.jpg',
+        descripcion: 'Preto'
+    },
+    {
+        nombre: 'Café Colombiano',
+        precio: 4000,
+        img: 'img/paqueteCafe1.jpg',
+        descripcion: 'EIOU PARCE'
+    },
+];
